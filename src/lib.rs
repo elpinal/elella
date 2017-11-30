4ce83b50ff0736f2e1bd03102e33462aee05d8ce
elpinal <elpinal@users.noreply.github.com>
2017-11-30 21:06:23 +0900

    Add 'parser' module

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,4 @@
 #![warn(missing_docs)]
 
 pub mod lexer;
-
-use std::collections::HashMap;
-
-enum Expr {
-    App(Box<Expr>, Vec<Expr>),
-    Lit(lexer::Lit),
-    List(Vec<Expr>),
-    Vec(Vec<Expr>),
-    Map(HashMap<String, Expr>),
-    If(Box<Expr>, Box<Expr>, Box<Expr>),
-}
+pub mod parser;
---
+++ src/parser.rs
@@ -0,0 +1,58 @@
+//! A parser for the Elella format.
+//!
+//! ```
+//! use elella::parser::*;
+//!
+//! let mut p = Parser::new(r#"[:a "x"]"#.as_bytes());
+//! assert_eq!(
+//!     p.parse().ok(),
+//!     Some(Expr::Vec(vec![
+//!         Expr::Lit(Lit::Keyword(String::from("a"))),
+//!         Expr::Lit(Lit::Str(String::from("x")))
+//!     ])));
+//! ```
+
+use std::collections::HashMap;
+use std::io::Read;
+
+use lexer::*;
+
+pub use lexer::Lit;
+
+#[derive(Debug, PartialEq)]
+pub enum Expr {
+    App(Box<Expr>, Vec<Expr>),
+    Lit(Lit),
+    List(Vec<Expr>),
+    Vec(Vec<Expr>),
+    Map(HashMap<String, Expr>),
+    If(Box<Expr>, Box<Expr>, Box<Expr>),
+}
+
+pub struct Parser<R: Read> {
+    l: Lexer<R>,
+}
+
+impl<R: Read> Parser<R> {
+    pub fn new(r: R) -> Parser<R> {
+        Parser { l: Lexer::new(r) }
+    }
+
+    pub fn parse(&mut self) -> Result<Expr, LexError> {
+        match self.l.lex()? {
+            Token::LBrack => self.vector(),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn vector(&mut self) -> Result<Expr, LexError> {
+        let mut vec = Vec::new();
+        loop {
+            match self.l.lex()? {
+                Token::RBrack => return Ok(Expr::Vec(vec)),
+                Token::Lit(l) => vec.push(Expr::Lit(l)),
+                _ => unimplemented!(),
+            }
+        }
+    }
+}
